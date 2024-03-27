3c854c3f0139743af5d9b2ce8bb9dd7bd712bb1c
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-03-27 08:17:01 -0400

    modified models and init to include db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 #Creates an instance of Flask called 'app'
 app = Flask(__name__)
+
+#Set the configuration
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
 
 #import the routes
 from .import routes
---
+++ app/models.py
@@ -0,0 +1,20 @@
+from . import db
+from datetime import datetime, timezone, timedelta
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    dueDate = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc) + timedelta(days=+1))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return f"<Title {self.id}|{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
