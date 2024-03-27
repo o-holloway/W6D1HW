668e6a328caa4f0904f5cfec744c417cd4035a65
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-03-27 07:50:30 -0400

    post request for tasks

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from fake_data.tasks import tasks_list
 from flask import request
 
+tasks = []
+
 #Define route
 @app.route('/')
 def index():
@@ -24,27 +26,31 @@ def get_task(task_id):
 
 #Create a task
 @app.route('/tasks', methods=['POST'])
-def create_task():
+def  create_task():
     if not request.is_json:
-        return {'error': f"Content must be application/json"},400
+        return {'error': f"Content-type must be application/json"},400
     data = request.json
-    required_fields = ['title','description']
+    required_fields = ['title', 'description', 'dueDate']
     missing_fields = []
     for field in required_fields:
         if field not in data:
             missing_fields.append(field)
     if missing_fields:
-        return {'error': f"{', '.join(missing_fields)} must be in the request body"},400
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
     title = data.get('title')
     description = data.get('description')
+    completed = False
+    createdAt = '2024-03-25T15:21:35'
+    dueDate = data.get('dueDate')
+        
     new_task = {
-        'id': len(tasks_list) + 1,
-        'title': title,
-        'description': description,
-        'userId': 1,
-        'dueDate': "2024-03-27T15:21:35",
-        'createdAt': "2024-03-25T15:21:35",
-        'completed': False
+        "id": len(tasks) + 1,
+        "title": title,
+        "description": description,
+        "completed": completed,
+        "createdAt": createdAt,
+        "dueDate": dueDate
     }
-    tasks_list.append(new_task)
+    tasks.append(new_task)
+
     return new_task, 201
