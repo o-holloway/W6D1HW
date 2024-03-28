722f8e67e423ad41491beba9fe75ab1d8e688121
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-03-28 08:51:51 -0400

    adjusted models.py

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    dueDate = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc) + timedelta(days=+1))
+    createdAt = db.Column(db.DateTime, nullable=False, default=lambda:datetime.now(timezone.utc))
+    dueDate = db.Column(db.DateTime, nullable=False, default=lambda:datetime.now(timezone.utc) + timedelta(days=+1))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,3 +18,13 @@ class Task(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed": self.completed,
+            "createdAt": self.createdAt,
+            "dueDate": self.dueDate
+        }
