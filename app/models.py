ce500fd2d10ef5ce29f7452556aa69c790326b61
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-04-01 06:46:28 -0400

    adjusted models to incorporate new auth module

--- app/models.py
+++ app/models.py
@@ -1,5 +1,56 @@
+import secrets
 from . import db
 from datetime import datetime, timezone, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    token = db.Column(db.String, index=True, unique=True)
+    token_expiration = db.Column(db.DateTime(timezone=True))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def set_password(self, plaintext_password):
+        self.password = generate_password_hash(plaintext_password)
+        self.save()
+
+    def check_password(self, plaintext_password):
+        return check_password_hash(self.password, plaintext_password)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "dateCreated": self.date_created
+        }
+    
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "tokenExpiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_expiration}
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,7 +70,11 @@ class Task(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
-    
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -29,3 +84,12 @@ class Task(db.Model):
             "createdAt": self.createdAt,
             "dueDate": self.dueDate
         }
+        
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "tokenExpiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_expiration}
